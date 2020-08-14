b810b16eda8b5e917ecdc06c28db346768dce16e
daniels_liu <daniels_liu@mtelnet.com>
2020-08-14 11:50:30 +0800

    update Demo

--- Demo/ViewController2.swift
+++ Demo/ViewController2.swift
@@ -60,7 +60,7 @@ extension ViewController2 {
         guard count > 0 else { return }
         let index = count - 1
         guard let task = sessionManager.tasks.safeObject(at: index) else { return }
-        // tableView 刷新、 删除 task 都是异步的，如果操作过快会导致数据不一致，所以需要限制button的点击
+        // tableView 刷新、 删除 task 都是异步的，如果操作过快会导致数据不一致，所以需要限制 button 的点击
         sender.isEnabled = false
         sessionManager.remove(task, completely: false) { [weak self] _ in
             self?.tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
